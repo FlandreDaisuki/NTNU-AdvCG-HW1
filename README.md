d8c2213b4fa670799b2b9a2521ef694dc5106a7b
FlandreDaisuki <vbnm123c@gmail.com>
2017-03-14 22:44:42 +0800

    v0.3
    truely ray-sphere instersection
    file input
    illust for depth

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # NTNU-AdvCG-HW1
+
+detail: http://cg.csie.ntnu.edu.tw/AdvCG/Assignment1.pdf
+
+## Requirement
+
+```shell
+$ pip install numpy scipy
+```
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+import json
+from pprint import pprint
+
 import numpy as np
+from scipy.misc import imshow
 
 def normalize(v):
     return np.array(v) / np.linalg.norm(v)
 
-def rotateMatFrom2Vecs(v1, v2):
+def data2rad(s):
+    """
+    data['F'] := <number> <deg|rad>
+    """
+    t = s.split(' ')
+    return float(t[0]) if t[1] == 'rad' else (float(t[0]) * np.pi / 180)
+
+def rotateMatFromNegZ(vec):
     # http://math.stackexchange.com/questions/180418/calculate-rotation-matrix-to-align-vector-a-to-vector-b-in-3d
-    v1 = normalize(v1)
-    v2 = normalize(v2)
+    v1 = np.array([0.0, 0.0, -1.0])
+    v2 = normalize(vec)
     v = np.cross(v1, v2)
     s = np.linalg.norm(v)
     c = np.dot(v1, v2)
@@ -14,41 +25,66 @@ def rotateMatFrom2Vecs(v1, v2):
     R = np.eye(3) + vx + vx * vx * (1 - c) / (s * s)
     return R
 
-def raySphereIntersection(ray, sphere):
+def isRaySphereIntersected(ray, sphere):
     # https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
-    oc = np.subtract(ray['o'], sphere['c'])
-    _1 = np.power(np.dot(normalize(ray['d']), oc), 2)
-    _2 = np.power(np.linalg.norm(oc), 2)
-    d = _1 - _2 + np.power(sphere['r'], 2)
-    return d >= 0
+    c, r = sphere['o'], sphere['r']
+    o, l = ray['o'], normalize(ray['d'])
+    oc = np.subtract(o, c)
+    _1 = np.dot(l, oc) ** 2
+    _2 = np.linalg.norm(oc) ** 2
+    D = _1 - _2 + r ** 2
+    if D < 0:
+        return False
+    dsmall = -(np.dot(l, oc)) - D ** 0.5
+    return True if dsmall >= 0 else False
+
+def isIntersected(ray, objectList):
+    spheres = objectList['S']
+    for sphere in spheres:
+        if isRaySphereIntersected(ray, sphere):
+            return True
+
+    triangles = objectList['T']
+    for triangle in triangles:
+        pass
+
+    return False
+
+
 
 def main():
-    eyePos = [0.0, 0.0, 0.0]
-    viewDir = [0.1, 0.1, -1.0] # OpenGL coordinate system
-    resolution = {'w':800, 'h':600}
-    fov = 100.0 * np.pi / 180.0 # rad
-    sphere = {'c':[0.0, 0.0, -10.0], 'r': 2.0} # x, y, z, r
+    # read and parse data
+    with open('input.json') as inputFile:
+        data = json.load(inputFile)
+    pprint(data)
 
-    depth = (1 / np.tan(fov / 2)) * np.hypot(resolution['w'], resolution['h'])/2
+    eyePos = np.array(data['E'])
+    viewDir = np.array(data['V']) # OpenGL coordinate system
+    resolution = data['R']
+    fov = data2rad(data['F']) # radian
+    spheres = data['S']
+    triangles = data['T']
 
-    ww = (resolution['w'] // 2) * 2
-    hh = (resolution['h'] // 2) * 2
-    canvas = np.zeros((hh, ww))
+    # pre-processing
+    rh, rw = resolution['h'], resolution['w']
+    depth = (1 / np.tan(fov / 2)) * np.hypot(rw, rh) / 2 # depth.png
+    hrange = -(np.arange(rh) - rh//2) # from + h/2 to -h/2
+    wrange = np.arange(rw) - rw//2
+    canvas = np.zeros((rh, rw))
+    objectList = {'S': spheres, 'T': triangles}
 
-    Rmatrix = rotateMatFrom2Vecs([0.0, 0.0, -1.0], viewDir)
+    # All vectors will rotate from (.0, .0, -1.0) to viewDir
+    Rmatrix = rotateMatFromNegZ(viewDir)
 
-    hi = 0
-    for h in np.arange(hh//2, -hh//2, -1):
-        wi = 0
-        for w in np.arange(-ww//2, ww//2, 1):
+    hr0, wr0 = hrange[0], wrange[0]
+    for h in hrange:
+        for w in wrange:
+            (hi, wi) = (hr0 - h, w - wr0)
             vec = normalize(np.matrix([w, h, -depth]))
             ray = {'o': eyePos, 'd': (Rmatrix * vec.T).T}
-            canvas[hi][wi] = 1 if raySphereIntersection(ray, sphere) else 0
-            wi = wi + 1
-        hi = hi + 1
+            canvas[hi][wi] = 1 if isIntersected(ray, objectList) else 0
 
-    from scipy import misc
-    misc.imshow(canvas)
+    imshow(canvas)
 
 if __name__ == '__main__':
     main()
