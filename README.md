206c52870ac953e4b5eaa91aa8b4e0f5a2fd3eb2
FlandreDaisuki <vbnm123c@gmail.com>
2017-03-15 01:11:12 +0800

    v1.0
    Done all features

--- README.md
+++ README.md
@@ -3,7 +3,17 @@
 detail: http://cg.csie.ntnu.edu.tw/AdvCG/Assignment1.pdf
 
 ## Requirement
+- pyenv
+  - python 3.x
+    - numpy
+    - scipy
 
+## Q&A
+
+Q: ImportError: No module named '_tkinter'
 ```shell
-$ pip install numpy scipy
+# http://stackoverflow.com/questions/26357567/cannot-import-tkinter-after-installing-python-3-with-pyenv
+
+$ sudo apt-get install tk-dev
+$ pyenv install $YOUR_PYTHON3_VERSION
 ```
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
 from pprint import pprint
+from shutil import copy as cp
+from os import makedirs
+import time
 
 import numpy as np
-from scipy.misc import imshow
+from scipy.misc import imshow, imsave
 
 def normalize(v):
     return np.array(v) / np.linalg.norm(v)
@@ -21,8 +24,14 @@ def rotateMatFromNegZ(vec):
     v = np.cross(v1, v2)
     s = np.linalg.norm(v)
     c = np.dot(v1, v2)
+
+    # too little rotation or too near 180 deg rotation
+    if s ** 2 < 1e-6 or np.abs(1 - c) < 1e-6:
+        sign = np.sign(c) or 1 # 1 for 0
+        return np.matrix(sign * np.eye(3))
+
     vx = np.matrix([[0.0, -v[2], v[1]], [v[2], 0.0, -v[0]], [-v[1], v[0], 0.0]])
-    R = np.eye(3) + vx + vx * vx * (1 - c) / (s * s)
+    R = np.eye(3) + vx + (vx ** 2) / (1 + c)
     return R
 
 def isRaySphereIntersected(ray, sphere):
@@ -38,6 +47,31 @@ def isRaySphereIntersected(ray, sphere):
     dsmall = -(np.dot(l, oc)) - D ** 0.5
     return True if dsmall >= 0 else False
 
+def isRayTriangleIntersected(ray, triangle):
+    # https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+    epsilon = 1e-6
+    v1, v2, v3 = np.array(triangle)
+    O, D = ray['o'], ray['d']
+    e1 = v2 - v1
+    e2 = v3 - v1
+    P = np.cross(D, e2)
+    det = np.dot(e1, P)
+    if det > -epsilon and det < epsilon:
+        return False
+    inv_det = 1 / det
+
+    T = O - v1
+    u = np.dot(T, P) * inv_det
+    if u < 0 or u > 1:
+        return False
+    Q = np.cross(T, e1)
+    v = np.dot(D, Q) * inv_det
+    if v < 0 or u + v > 1:
+        return False
+
+    t = np.dot(e2, Q) * inv_det
+    return t > epsilon
+
 def isIntersected(ray, objectList):
     spheres = objectList['S']
     for sphere in spheres:
@@ -46,11 +80,10 @@ def isIntersected(ray, objectList):
 
     triangles = objectList['T']
     for triangle in triangles:
-        pass
+        if isRayTriangleIntersected(ray, triangle):
+            return True
 
     return False
-
-
 
 def main():
     # read and parse data
@@ -81,9 +114,15 @@ def main():
         for w in wrange:
             (hi, wi) = (hr0 - h, w - wr0)
             vec = normalize(np.matrix([w, h, -depth]))
-            ray = {'o': eyePos, 'd': (Rmatrix * vec.T).T}
+            ray = {'o': np.array(eyePos), 'd': (Rmatrix * vec.T).A1}
             canvas[hi][wi] = 1 if isIntersected(ray, objectList) else 0
 
+    # output results
+    timeInt = int(time.time())
+    pathname = 'results/{}/'.format(timeInt)
+    makedirs(pathname)
+    imsave(pathname + 'result.png', canvas)
+    cp('input.json', pathname)
     imshow(canvas)
 
 if __name__ == '__main__':
